9ef92e431322756be6dfc31441c1ef56b3779c6a
Maksym Melnychuk <maksym.melnychuk@dedicatted.com>
2022-06-29 16:18:20 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import firebase from './FirebaseConfig';
+
+import './App.css';
+
+const App = () => {
+    return (
+        <div className="App">
+            <div className="title-row">
+                <h1 className="title">Firebase Recipes</h1>
+            </div>
+        </div>
+    );
+};
+
+export default App;
